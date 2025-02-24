cce837dbe92ddceb4b673916dc09ad861dd636f0
Joseph Teka <179788482+Josy216@users.noreply.github.com>
2025-02-24 14:45:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector("#more");
+const morePro = document.querySelector(".more-pro");
+btn.addEventListener("click", function more() {
+  morePro.style.display = "flex";
+});
